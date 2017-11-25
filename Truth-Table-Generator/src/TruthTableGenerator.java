e9a03164155d47d2d4182d7dcd663537ba395736
Youssef_Ali <youssefali6212@gmail.com>
2017-11-25 13:58:46 +0200

    Initializing Java Project

---
+++ Truth-Table-Generator/src/TruthTableGenerator.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class TruthTableGenerator {
+
+    ArrayList<String> expressions;
+    ArrayList<Boolean[][]> truthTables;
+
+
+    public TruthTableGenerator(){
+        expressions = new ArrayList<>();
+        truthTables = new ArrayList<>();
+    }
+
+    public Boolean[][] generateTruthTable(String expression){
+
+    }
+}
